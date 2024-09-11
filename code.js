5783983938525e8135518c4559092e12d957911e
daridev <darideveloper@gmail.com>
2024-09-11 13:06:13 -0600

    feat (indes-hero): layout & responsive

---
+++ code.js
@@ -0,0 +1,9 @@
+// var animationTestHero = anime({
+//   targets: '.index-hero h1 span',
+//   translateY: 30,
+//   opacity: 1,
+//   loop: true,
+//   delay: function(el, i) { return i * 200; },
+//   easing: 'easeInOutSine',
+//   autoplay: true,
+// });
