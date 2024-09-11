4c54ca9c8216b5750d7036e6fb870dcae7409a76
daridev <darideveloper@gmail.com>
2024-09-11 13:42:40 -0600

    feat (index-hero): animations

--- code.js
+++ code.js
@@ -1,9 +1,47 @@
-// var animationTestHero = anime({
-//   targets: '.index-hero h1 span',
-//   translateY: 30,
-//   opacity: 1,
-//   loop: true,
-//   delay: function(el, i) { return i * 200; },
-//   easing: 'easeInOutSine',
-//   autoplay: true,
-// });
+// Index hero
+
+// h1 animation
+var animationH1Hero = anime({
+  targets: '.index-hero h1 span',
+  opacity: 1,
+  delay: function(el, i) { return i * 200; },
+  easing: 'easeInOutSine',
+  autoplay: true,
+  translateX: {
+    value: [50, 0],
+  },
+  opacity: {
+    value: [0, 1],
+  }
+});
+
+// text animation
+var animationTextHero = anime({
+  targets: '.index-hero p',
+  opacity: 1,
+  delay: 1600,
+  easing: 'easeInOutSine',
+  autoplay: true,
+  translateX: {
+    value: [50, 0],
+  },
+  opacity: {
+    value: [0, 1],
+  }
+});
+
+// lines animation
+let baseHeight = 100
+if (window.matchMedia('(max-width: 768px)').matches) {
+  baseHeight = 50
+}
+var animationTextHero = anime({
+  targets: '.index-hero .line-inner',
+  opacity: 1,
+  delay: [1800, function(el, i) { return 1800 + i * 100; }],
+  easing: 'easeInOutSine',
+  autoplay: true,
+  height: {
+    value: [0, function(el, i) { return (i + 1) * baseHeight; }],
+  },
+});
